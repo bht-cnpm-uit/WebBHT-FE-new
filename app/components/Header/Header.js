923bcecf63041c9404059600994ae9deae2229f9
Minh Chau <buitongminhchau01032002@gmail.com>
2023-02-28 23:06:23 +0700

    update header resize

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,35 +1,60 @@
 'use client';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useMotionValueEvent, motion, useScroll, useTransform, useAnimationControls } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 import NavLink from '../NavLink';
 import MobileMenu from './MobileMenu';
 import DesktopNav from './DesktopNav';
 
 export default function Header({ platformGroup }) {
-    const [inTop, setInTop] = useState(true);
+    const [isInTop, setIsInTop] = useState(true);
+    const { scrollY } = useScroll();
 
-    useEffect(() => {
-        function handler() {
-            const offsetY = window.pageYOffset || document.documentElement.scrollTop;
-            setInTop(offsetY < 80);
+    const headerHeight = useTransform(scrollY, [0, 50], ['90px', '64px']);
+
+    // const controls = useAnimationControls();
+    // const delta = useRef(0);
+    // const lastScrollY = useRef(0);
+    useMotionValueEvent(scrollY, 'change', (val) => {
+        // const diff = Math.abs(val - lastScrollY.current);
+        // if (val >= lastScrollY.current) {
+        //     delta.current = delta.current >= 10 ? 10 : delta.current + diff;
+        // } else {
+        //     delta.current = delta.current <= -10 ? -10 : delta.current - diff;
+        // }
+
+        // if (delta.current >= 10 && val > 200) {
+        //     controls.start('hidden');
+        // } else if (delta.current <= -10 || val < 200) {
+        //     controls.start('visible');
+        // }
+        if (val > 100) {
+            setIsInTop(false);
+        } else {
+            setIsInTop(true);
         }
-        if (typeof window !== 'undefined') {
-            window.addEventListener('scroll', handler);
-        }
-        return () => {
-            window.removeEventListener('scroll', handler);
-        };
-    }, []);
+        // lastScrollY.current = val;
+    });
 
     return (
-        <header
+        <motion.header
             className={clsx(
-                'fixed top-0 z-header flex h-h-header w-full items-center justify-between bg-bg px-p-body transition-shadow duration-300',
+                'fixed top-0 z-header flex w-full items-center justify-between bg-bg px-p-body transition-shadow duration-300',
                 {
-                    'border-gray-200 shadow-lg shadow-gray-600/5 dark:shadow-gray-500/5': !inTop,
+                    'border-gray-200 shadow-lg shadow-gray-600/5 dark:shadow-gray-500/5': !isInTop,
                 }
             )}
+            initial="visible"
+            // animate={controls}
+            // transition={{ duration: 0.2 }}
+            // variants={{
+            //     visible: { y: '0px' },
+            //     hidden: { y: '-90px' },
+            // }}
+            style={{
+                height: headerHeight,
+            }}
         >
             {/* LOGO */}
             <div className="flex flex-1 items-center md:flex-initial">
@@ -46,6 +71,6 @@ export default function Header({ platformGroup }) {
 
             {/* MOBILE GROUP */}
             <MobileMenu platformGroup={platformGroup} />
-        </header>
+        </motion.header>
     );
 }
