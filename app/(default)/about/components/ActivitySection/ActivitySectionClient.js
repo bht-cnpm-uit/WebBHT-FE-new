f193fe9f452990a216636a13b0d1be9c2169a730
Minh Chau <buitongminhchau01032002@gmail.com>
2023-03-02 17:13:25 +0700

    fix error heading

--- app/(default)/about/components/ActivitySection/ActivitySectionClient.js
+++ app/(default)/about/components/ActivitySection/ActivitySectionClient.js
@@ -14,10 +14,10 @@ export default function ActivitySectionClient({ activities, heading }) {
                     <h2
                         className="heading-section"
                         dangerouslySetInnerHTML={{
-                            __html: heading.heading || '',
+                            __html: heading?.heading || '',
                         }}
                     ></h2>
-                    <h3 className="text-lg sm:text-base">{heading.description || ''}</h3>
+                    <h3 className="text-lg sm:text-base">{heading?.description || ''}</h3>
                 </motion.header>
                 <div className="mt-24">
                     {activities?.map((activity, index) => (
