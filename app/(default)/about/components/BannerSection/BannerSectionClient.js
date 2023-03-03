c0acecdad31862e3acef4e4f7c9134433640a8aa
Minh Chau <buitongminhchau01032002@gmail.com>
2023-03-03 12:08:31 +0700

    animation banner

--- app/(default)/about/components/BannerSection/BannerSectionClient.js
+++ app/(default)/about/components/BannerSection/BannerSectionClient.js
@@ -1,10 +1,15 @@
 'use client';
-
+import { motion } from 'framer-motion';
 import ParseNotionPageContent from '~/app/components/ParseNotionPageContent';
 
 export default function BannerSectionClient({ heading, description }) {
     return (
-        <div className="flex flex-col items-center justify-center px-p-body pb-16 pt-12">
+        <motion.div
+            className="flex flex-col items-center justify-center px-p-body pb-16 pt-12"
+            initial={{ y: 50, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+        >
             <h2
                 className="mb-7 max-w-[620px] text-center text-5xl font-bold leading-tight text-text-dark xs:text-4xl"
                 dangerouslySetInnerHTML={{ __html: heading }}
@@ -12,6 +17,6 @@ export default function BannerSectionClient({ heading, description }) {
             <div className="max-w-[777px] text-center text-lg xs:text-base">
                 <ParseNotionPageContent>{description || []}</ParseNotionPageContent>
             </div>
-        </div>
+        </motion.div>
     );
 }
