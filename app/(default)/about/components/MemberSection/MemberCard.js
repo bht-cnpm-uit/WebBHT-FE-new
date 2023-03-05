70ebb92ca324027b12d0d11589426883d637b0d1
Minh Chau <buitongminhchau01032002@gmail.com>
2023-03-05 15:07:59 +0700

    add animation member section

--- app/(default)/about/components/MemberSection/MemberCard.js
+++ app/(default)/about/components/MemberSection/MemberCard.js
@@ -1,16 +1,25 @@
 'use client';
+import { motion } from 'framer-motion';
+
+const memberVariants = {
+    hidden: { x: -10, opacity: 0 },
+    visible: {
+        opacity: 1,
+        x: 0,
+    },
+};
 
 function MemberCard({ member }) {
     return (
         <>
-            <div className="group">
+            <motion.div variants={memberVariants} className="group">
                 <div className="m-3 h-24 w-24 overflow-hidden rounded-full ring-primary ring-offset-2 sm:m-2 sm:h-20 sm:w-20 can-hover:group-hover:ring-2">
                     <img
                         src={member.image || '/images/avatar_placeholder.png'}
                         className="h-full w-full object-cover"
                     />
                 </div>
-            </div>
+            </motion.div>
         </>
     );
 }
--- app/(default)/about/components/MemberSection/MemberGroup.js
+++ app/(default)/about/components/MemberSection/MemberGroup.js
@@ -1,11 +1,76 @@
+import { motion } from 'framer-motion';
 import MemberCard from './MemberCard';
+
+const container = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            delayChildren: 0,
+        },
+    },
+};
+
+const yearVariants = {
+    hidden: { x: 50, opacity: 0 },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+        },
+    },
+};
+
+const circleVariants = {
+    hidden: {
+        pathLength: 0,
+    },
+    visible: {
+        pathLength: 1,
+        transition: {
+            duration: 0.5,
+            ease: 'easeInOut',
+        },
+    },
+};
+
+const lineVariants = {
+    hidden: { height: 0 },
+    visible: {
+        height: 'initial',
+        transition: {
+            duration: 0.3,
+            delay: 0.5,
+        },
+    },
+};
+
+const membersVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            delay: 0.7,
+            when: 'beforeChildren',
+            staggerChildren: 0.1,
+        },
+    },
+};
 
 export default function MemeberGroup({ members, year }) {
     return (
-        <div className="mt-10 w-full">
+        <motion.div
+            variants={container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ margin: '0px 0px -200px 0px' }}
+            className="mt-10 w-full"
+        >
+            {/* YEAR */}
             <div className="-ml-[15px] flex items-center">
                 <div className="rounded-full text-primary">
-                    <svg
+                    <motion.svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -13,22 +78,33 @@ export default function MemeberGroup({ members, year }) {
                         stroke="currentColor"
                         className="h-8 w-8"
                     >
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
+                        <motion.path
+                            variants={circleVariants}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </motion.svg>
                 </div>
 
-                <div className="ml-7 text-4xl font-bold text-text-semidark">{year}</div>
+                <motion.div variants={yearVariants} className="ml-7 text-4xl font-bold text-text-semidark">
+                    {year}
+                </motion.div>
             </div>
+
             <div className="flex items-stretch">
                 {/* Line */}
-                <div className="-my-2 w-0.5 rounded-full bg-gradient-to-b from-primary via-primary-to to-primary-to/0"></div>
+                <motion.div
+                    variants={lineVariants}
+                    className="-my-2 w-0.5 rounded-full bg-gradient-to-b from-primary via-primary-to to-primary-to/0"
+                ></motion.div>
                 {/* CARDS */}
-                <div className="ml-7 mt-5 flex flex-1 flex-wrap">
+                <motion.div variants={membersVariants} className="ml-7 mt-5 flex flex-1 flex-wrap">
                     {members?.map((member, index) => (
                         <MemberCard key={index} member={member} />
                     ))}
-                </div>
+                </motion.div>
             </div>
-        </div>
+        </motion.div>
     );
 }
