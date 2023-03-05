4365639eeaf17ea61f72cd4c3c2a9152de15eaf6
Minh Chau <buitongminhchau01032002@gmail.com>
2023-03-05 16:19:23 +0700

    fix outline member card

--- app/(default)/about/components/MemberSection/MemberCard.js
+++ app/(default)/about/components/MemberSection/MemberCard.js
@@ -12,7 +12,7 @@ function MemberCard({ member }) {
     return (
         <>
             <motion.div variants={memberVariants} className="group">
-                <div className="m-3 h-24 w-24 overflow-hidden rounded-full ring-primary ring-offset-2 sm:m-2 sm:h-20 sm:w-20 can-hover:group-hover:ring-2">
+                <div className="m-3 h-24 w-24 overflow-hidden rounded-full outline-2 outline-offset-2 outline-primary sm:m-2 sm:h-20 sm:w-20 can-hover:group-hover:outline">
                     <img
                         src={member.image || '/images/avatar_placeholder.png'}
                         className="h-full w-full object-cover"
