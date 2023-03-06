4d6e500160ef9b8743d187583a2d62be43aa439b
Minh Chau <buitongminhchau01032002@gmail.com>
2023-03-06 19:37:33 +0700

    update thumbnail

--- app/(default)/layout.js
+++ app/(default)/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
         siteName: 'Ban học tập Công nghệ Phần mềm',
         images: [
             {
-                url: '/images/thumbnail.png',
+                url: '/images/thumbnail_bht.png',
             },
         ],
         type: 'website',
